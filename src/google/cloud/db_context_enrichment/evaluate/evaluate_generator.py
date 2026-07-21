13edc79db219bc02512819f40fd978ff3f69d1f5
totoleon <hailong.leon@gmail.com>
2026-07-21 17:22:26 +0000

    Add BigQuery support to evaluation config generation
    Implements the BigQuery portion of the Wave2 FR:
    - Add BigQueryConfigGenerator mapping tools.yaml bigquery sources to
      Evalbench db_config and GDA BigQueryTableReferences model config
    - Register the generator in the evaluate_generator factory map
    - Add bigquery.md connection reference template for the init skill
    - Attach agent_context_reference dynamically since the field is not yet
      in the public GDA SDK (restricted-visibility rollout)

--- src/google/cloud/db_context_enrichment/evaluate/evaluate_generator.py
+++ src/google/cloud/db_context_enrichment/evaluate/evaluate_generator.py
@@ -10,6 +10,7 @@ from google.cloud.db_context_enrichment.common import config
 
 from .db_generators.alloydb import AlloyDBConfigGenerator
 from .db_generators.base import BaseDBConfigGenerator
+from .db_generators.bigquery import BigQueryConfigGenerator
 from .db_generators.bigtable import BigtableConfigGenerator
 from .db_generators.firestore import FirestoreConfigGenerator
 from .db_generators.mysql import MySQLConfigGenerator
@@ -211,6 +212,7 @@ def _get_db_generator(params: dict[str, Any]) -> BaseDBConfigGenerator:
 
     generators = {
         AlloyDBConfigGenerator.SOURCE_TYPE: AlloyDBConfigGenerator,
+        BigQueryConfigGenerator.SOURCE_TYPE: BigQueryConfigGenerator,
         BigtableConfigGenerator.SOURCE_TYPE: BigtableConfigGenerator,
         PostgresConfigGenerator.SOURCE_TYPE: PostgresConfigGenerator,
         MySQLConfigGenerator.SOURCE_TYPE: MySQLConfigGenerator,
